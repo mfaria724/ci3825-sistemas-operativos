bf2f1ca1f9de1e7e6541aa1e8a9b3096523597d3
Jesus Kauze <12-10273@usb.ve>
2019-02-23 17:28:46 -0400

    Corregido condiciones de insertar elemento/nodo

--- main.c
+++ main.c
@@ -36,7 +36,7 @@ tLista Crear(){
     tLista l;                               //inicialista la lista l
     l = (tLista)malloc(sizeof(tiponodo));   //Asigna espacio que requiere l para tiponodo
     if (l == NULL)
-        error("Memoria llena");
+        error("Memoria insuficiente");
     l->siguiente = l->anterior = l;         //define la conexion circular de l
     return l;
 }
@@ -47,7 +47,7 @@ tPosicion Primero (tLista l){
 }
 
 // Retorna Ultimo elemento (usados por funcion Posicion)
-tPosicion Fin (tLista l){    	
+tPosicion Fin (tLista l){
    return l;
 }
 
@@ -64,37 +64,54 @@ void Insertar (int fecha, char *mensaje, char *cifrado, tPosicion p, tLista l){
     nuevo_elemento->anterior = p->anterior;
     p->anterior->siguiente=nuevo_elemento;
     p->anterior = nuevo_elemento;
-    printf("\nContenido de la lista %d, con conteido %s y cifrado %s", nuevo_elemento->fecha, nuevo_elemento->mensaje, nuevo_elemento->cifrado);
+    printf("Contenido del elemento: fecha %d | conteido %s | cifrado %s | siguiente DESHABILITADO para estas impresiones\n", nuevo_elemento->fecha, nuevo_elemento->mensaje, nuevo_elemento->cifrado, nuevo_elemento->siguiente->fecha);
+    return;
 }
 
 // Retorna la posicion en la que se encuentra un nodo (busca por fecha)
+// 1) si la fecha de la primera posicion es mayor que la fecha introducida, entonces la fecha introducida sera el primer elemento/nodo.
+// 2) si la fecha introducida es >= que la fecha de la posicion p y <= que la fecha de la posicion siguiente, entonces anadir en la posicion p.siguiente la nueva fecha.
 tPosicion posicion (int fecha, tLista l){
    tPosicion p;
    int encontrado;
+   int contador_posicion;
    p = Primero(l);
    encontrado = 0;
+   printf("primera fecha del primer nodo: %d | nodo siguiente: %d (esto es de la funcion posicion)\n", p->fecha, p->siguiente->fecha); //ELIMINAR ESTO ES UNICAMENTE PARA VERIFICAR BUEN MAPEO DE LA LISTA [CORRECTO]
+   if (p->fecha < fecha){                                     // *1)
    while ((p != Fin(l)) && (!encontrado))
-	if (p->fecha == fecha)
+	if (p->fecha <= fecha && p->siguiente->fecha >= fecha){   // *2)
+        p = p->siguiente;
 		encontrado = 1;
-	else
+    }else{
 		p = p->siguiente;
+    }
+   }
    return p;
 }
 
+int elemento (tPosicion p, tLista l){
+//     if (p == l){
+// 	    error("Posicion fin(l)");
+//    }
+   return p->fecha;
+}
+
 // Toma lo valores necesarios para anadir un nuevo_elemento a la tLista l
-int registro(tLista l) {
+void registro(tLista l) {
     int fecha = 0;
     char mensaje[10];
     char cifrado[10];
     printf("\nIngrese su fecha YYYYmmdd: ");
     scanf("%d", &fecha);
-    printf("\nIngrese su mensaje: ");
+    printf("Ingrese su mensaje: ");
     scanf("%s", &mensaje);
     getchar();
-    printf("\nIngrese cifrado: ");
+    printf("Ingrese cifrado: ");
     scanf("%s",&cifrado);
     Insertar(fecha, mensaje, cifrado, posicion(fecha, l), l);
-    printf("\nRegistro\n");
+    printf("\nRegistro exitoso\n");
+    return;
 }
 
 int descifrar(){
@@ -105,8 +122,12 @@ int cifrar(){
     printf("\nCifrar\n");
 }
 
-int mostrar(){
+int mostrar(tLista l){          //NO ESTA IMPLEMENTANDO, UNICAMENTE PARA VERIFICAR EL BUEN MAPEO DE LA LISTA
+    int elementobuscar;
     printf("\nMostrar\n");
+    printf("ingrese fecha: ");     // PON CUALQUIER VALOR RANDOM
+    scanf("%d", &elementobuscar);
+    printf("Fecha del nodo: %d\n",elemento(posicion(elementobuscar,l),l));
 }
 
 int borrar(){
@@ -116,11 +137,11 @@ int borrar(){
 /* Main: Menu principal - Display para elegir una de las funciones del programa*/
 int main(){
     tLista l = Crear();
-    int opcion = 0;
-    while ( opcion == 0 || opcion > 6 ){
-    printf("Introduzca una opcion valida: (1)registro - (2)decifrar mensaje - (3)cifrar mensajes - (4)mostrar cifrados disponibles - (5)borrar cifrado \nOpcion: ");
+    int opcion;
+    // Mientras opcion sea <0 o >6 permanecer en el loop;
+    while ( opcion < 0 || opcion > 6 ){ 
+    printf("Introduzca una opcion valida: (1)registro - (2)decifrar mensaje - (3)cifrar mensajes - (4)mostrar cifrados disponibles - (5)Borrar cifrado - (0)Salir\nOpcion: ");
     scanf("%d", &opcion);
-    }
     printf("\nHas Elegido Opcion: %i\n", opcion);
     if ( opcion==1 ) {
         registro(l);
@@ -129,10 +150,13 @@ int main(){
     } else if ( opcion==3 ) {
         cifrar();
     } else if ( opcion==4 ) {
-        mostrar();
+        mostrar(l);     // NO ESTA IMPLEMENTADO UNICAMENTE PARA TESTEAR EL BUEN MAPEO DE LA LISTA
     } else if ( opcion==5 ) {
         borrar();
+    } else if ( opcion==0 ) {
+        break;
+      }
+      opcion = 8; // colocar cualquier valor aqui > 6
     }
-
     return 0;
 }
