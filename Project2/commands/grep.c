0073388cf3bdd0803df3c876793da7122a9f3dfe
juan <jaoc1811@gmail.com>
2019-06-07 00:37:48 -0400

    Add comments to program

--- Project2/commands/grep.c
+++ Project2/commands/grep.c
@@ -4,22 +4,24 @@
 
 void main(int argc, char *argv[]){
 
-  char temp[1024];
-  int options[3] = {0,0,0};
-  int combination;
+  // Initializes variables
+  char temp[1024];          // Buffer
+  int options[3] = {0,0,0};     // Checks every option
+  int combination;     // Used to get options from flags
 
-
+  // Initialize I/O files
   FILE *fp;
   FILE *out;
 
 
   if(argc > 1){
 
-    // Reading flags
+    // Iterates over the arguments
     for (int z = 1; z < argc-1 ; z++) {
 
       combination = strlen(argv[z]);
 
+      // If the argument is a flag, and has more than one option, iterates over it to get every option on it
       if (argv[z][0] == '-' && combination > 2 ) {
 
         char* flags = argv[z];
@@ -37,7 +39,8 @@ void main(int argc, char *argv[]){
             printf("Unrecognized operation -%c\n",argv[z][w]);
             return;
           }
-       }
+       } 
+      // If the argument has only one option, checks what option is it
       } else { 
         if (strcmp("-i", argv[z]) == 0) {
           options[0] = 1;
@@ -58,11 +61,14 @@ void main(int argc, char *argv[]){
     // Case Prompt
     if(argv[argc - 2][0] == '-' || argc == 2){
 
+      // Gets input from the user until he cancels it
       while(fgets(temp,1000,stdin)){
-
+        
+        // Condition to stop the cycle if the input if coming from another command
         if(strcmp(temp,"EndOfPipe\n") == 0)
           break;
         
+        // Verifies what option has to apply, then checks if the input matchs with the pattern to print it
         if(options[1]){
           if(options[0]){
             if(!strcasestr(temp,argv[argc - 1]))
@@ -85,7 +91,7 @@ void main(int argc, char *argv[]){
     // Case File    
     } else if(argv[argc - 2][0] != '-') {
         
-      // Counts arguments that are flags
+      // Counts arguments that are flags and verifies if the output will be redirectioned to a file
       int lastFlag = 0;
       for(int i = 0; i < argc - 1; i++){
 
@@ -98,13 +104,14 @@ void main(int argc, char *argv[]){
         }
       }
 
-
+      // Iterates over every file in the input to run grep on them
       for(int i = lastFlag + 2; i <= argc - 1; i++){
 
         // Jump to next iteration if has <
         if(strcmp("<",argv[i]) == 0)
           i++;
-          
+
+        // Checks when to finish the cycle if the output is going to be redirectioned 
         if(strcmp(">",argv[i]) == 0)
           break;
             
@@ -157,10 +164,12 @@ void main(int argc, char *argv[]){
               }  
             }
           }
-        }   
+        }
+        // Closes input file
         fclose(fp);
                
       }
+      // Closes output file
       if(options[2])
         fclose(out);
     }
